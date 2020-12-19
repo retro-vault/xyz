53fbc35b692149d21dc2df97515d1d879571dd24
tstih <tomaz.stih@post.com>
2020-12-19 23:30:16 +0000

    main include file update

--- yos/include/yos.h
+++ yos/include/yos.h
@@ -9,6 +9,19 @@
 #ifndef _YOS_H
 #define _YOS_H
 
+/* yos definitions */
+#define NULL    ( (word)0 )
+#define TRUE    ( (byte)1 )
+#define FALSE   ( (byte)0 )
+
+/* yos types */
+typedef signed char         byte;
+typedef byte                boolean;
+typedef byte                result;
+typedef unsigned short int  word;
+typedef word                handle;
+typedef byte*               string;
+
 /* API */
 typedef struct yos_s yos_t;
 struct yos_s {
@@ -29,7 +42,7 @@ struct yos_s {
 };
 
 /* call this to obtain the yos interface
- *   yos_t* api=(yos*)query_interface("yos");
+ *   yos_t* api=(yos*)query_interface("yo00"); // 2 letters + 2 dig. version
  *   void* mem_block=api->allocate(1024);
  * this function is defined in crt0.s for yos application
  */
